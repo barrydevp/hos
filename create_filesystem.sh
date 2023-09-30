65ce169cfe82f01c0eb3b75f640c77b9170d9326
barrydevp <barrydevp@gmail.com>
2023-10-01 01:21:26 +0700

    chore: rename rootfs in create_filesystem script

--- create_filesystem.sh
+++ create_filesystem.sh
@@ -2,12 +2,12 @@ echo '==========================================================================
 echo 'Creating EXT2 filesystem...'
 echo '============================================================================='
 
-mkdir -p ./files/proc
-mkdir -p ./files/dev
-mkdir -p ./files/usr
-echo "Hello Wolrd" >> ./files/usr/hello.txt
+mkdir -p ./rootfs/proc
+mkdir -p ./rootfs/dev
+mkdir -p ./rootfs/usr
+echo "Hello Wolrd" >> ./rootfs/usr/hello.txt
 
-mke2fs -L 'rootfs' -N 0 -d ./files -b 4096 -m 5 -r 1 -t ext2 -v -F ./rootfs.img 32M
+mke2fs -L 'rootfs' -N 0 -d ./rootfs -b 4096 -m 5 -r 1 -t ext2 -v -F ./rootfs.img 32M
 echo '============================================================================='
 echo 'Done!'
 echo '============================================================================='
